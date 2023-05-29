5704a4da5996aa7ef58fb1ec992cd7df878cf015
unknown <amantaphelix@gmail.com>
2023-05-29 21:23:57 +0530

    first commit

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import { AppBar, Button, Toolbar } from '@mui/material'
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+        <AppBar>
+            <Toolbar>
+                <Button></Button>
+            </Toolbar>
+        </AppBar>
+    </div>
+  )
+}
+
+export default NavBar
