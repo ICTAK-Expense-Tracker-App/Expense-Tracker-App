31983ae664fdbf7453add98ca8aa53ce862d23cf
ANN MARY WILSON <annmarywilson293@gmail.com>
2023-05-30 22:28:06 +0530

    hey

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,14 +1,15 @@
 import { AppBar, Button, Toolbar } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = () => {
   return (
     <div>
-        <AppBar>
+        <AppBar position="static" className="navbar">
             <Toolbar>
-            <Button component={Link} to='/Login' variant='contained'>Login</Button>
-          <Button component={Link} to='/SignUp' variant='contained'>Sign Up</Button>
+            <Button component={Link} to='/Login' variant='contained' className='customButton'>Login</Button>
+          <Button component={Link} to='/SignUp' variant='contained' className='customButton'>Sign Up</Button>
             </Toolbar>
         </AppBar>
     </div>
