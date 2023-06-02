f4cdcae6a483d9a7b06214b7e04a8b35d1641e65
ANN MARY WILSON <annmarywilson293@gmail.com>
2023-06-02 22:28:34 +0530

    hey

---
+++ backend/model/customer.js
@@ -0,0 +1,18 @@
+// import mongoose
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://annmarywilson:annmarywilson@cluster0.fwg4655.mongodb.net/?retryWrites=true&w=majority')
+
+// to check for error unimportant step
+.then(()=>{
+    console.log("db connected")
+})
+.catch(err=>console.log(err))
+
+let Schema = mongoose.Schema
+const customerSchema = new Schema({
+    cname:String,
+    cgrade:Number
+})
+var customerModel = mongoose.model("students",customerSchema);
+module.exports = customerModel
