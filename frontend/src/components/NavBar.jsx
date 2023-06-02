97bff24348039ac45d54eaa8776a5df79655a6aa
ANN MARY WILSON <annmarywilson293@gmail.com>
2023-06-02 23:06:44 +0530

    hey

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppBar, Button, Toolbar } from '@mui/material';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = () => {
+  const [buttonColor, setButtonColor] = useState(localStorage.getItem('buttonColor') || 'green');
+
+  useEffect(() => {
+    localStorage.setItem('buttonColor', buttonColor);
+  }, [buttonColor]);
+
   return (
     <div>
       <AppBar position="static" className="navbar">
         <Toolbar className="toolbar">
           <div className="spacer" />
-          <Button component={Link} to="/Login" variant="contained" className="customButton">Login</Button>
-          <Button component={Link} to="/SignUp" variant="contained" className="customButton">Sign Up</Button>
-          <Button component={Link} to="/Home" variant="contained" className="customButton">Home</Button>
+          <Button
+            component={Link}
+            to="/Login"
+            variant="contained"
+            className="customButton"
+            style={{ backgroundColor: buttonColor }}
+          >
+            Login
+          </Button>
+          <Button
+            component={Link}
+            to="/SignUp"
+            variant="contained"
+            className="customButton"
+            style={{ backgroundColor: buttonColor }}
+          >
+            Sign Up
+          </Button>
+          <Button
+            component={Link}
+            to="/Home"
+            variant="contained"
+            className="customButton"
+            style={{ backgroundColor: buttonColor }}
+          >
+            Home
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
