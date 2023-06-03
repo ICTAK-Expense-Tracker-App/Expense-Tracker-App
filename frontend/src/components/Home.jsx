e4865bbd32684dac2c19a93a2fe3ac04fcc71898
ANN MARY WILSON <annmarywilson293@gmail.com>
2023-06-03 08:47:49 +0530

    hey

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -9,15 +9,13 @@ import heyy from '../assets/heyy.jpg';
 const Home = () =>{
     return (
         <div className="homepage">
-                <div className="home-content">
                     <h1 className="home-heading">WELCOME TO HOME PAGE</h1>
                     <p>Here you can View and Manage your Income and Expenses.</p>
 
                     <Link to="/AddNew" className="home-link">Add New</Link>
                     <Link to="/Profile" className="home-link">Profile</Link>
                     <Link to="/Admin" className="home-link">Admin</Link>
-                </div>
-        
+
         </div>
     );
 }
