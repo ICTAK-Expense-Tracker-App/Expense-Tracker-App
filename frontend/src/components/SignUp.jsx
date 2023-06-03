9448c7a3605a80687ab532c6c1605934d39549d5
ANN MARY WILSON <annmarywilson293@gmail.com>
2023-06-03 10:49:36 +0530

    hey

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -3,11 +3,11 @@ import { Grid, Paper, Avatar, Typography, TextField, Button, FormControlLabel} f
 import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
 import { CheckBox } from '@mui/icons-material';
 import './SignUp.css'
-import money1 from '../assets/money1.jpg';
+import back from '../assets/back.jpg';
 import './SignUp.css';
 
 const SignUp = () => {
-    const paperStyle = { padding: 15, width: 300, margin: "0 auto" };
+    const paperStyle = { padding: 15, width: 500, margin: "0 auto" };
     const headerStyle = { margin: 0 };
     const avatarStyle = { backgroundColor: '#1bbd7e' };
 
@@ -35,7 +35,12 @@ const SignUp = () => {
                         control={<CheckBox name="checkedA" />}
                         label="I accept the terms and conditions."
                     />
+                    <br />
                     <Button type='submit' variant='contained' color='primary'>Sign up</Button>
+                    <div className="signup-link">
+                        <span>Already have an account?</span>
+                        <a href="http://localhost:3000/Login" className="black-link">Login to your account</a>
+                    </div>
                 </form>
             </Paper>
         </Grid>
