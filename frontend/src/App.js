84e1a91de0bdfda22f8ac6a5f281a86facb93007
ANN MARY WILSON <annmarywilson293@gmail.com>
2023-06-03 09:48:21 +0530

    hey

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,6 @@ import NavBar from './components/NavBar';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Home from './components/Home';
-import AddNew from './components/AddNew';
-import Profile from './components/Profile';
-import Admin from './components/Admin';
 
 function App() {
   return (
@@ -17,9 +14,6 @@ function App() {
         <Route path="/Login" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/" element={<Home />} />
-        <Route path="/AddNew" element={<AddNew />} />
-        <Route path="/Profile" element={<Profile />} />
-        <Route path="/Admin" element={<Admin />} />
       </Routes>
     </div>
   );
