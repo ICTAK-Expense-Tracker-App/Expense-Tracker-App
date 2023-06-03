936a7686603d27c75d65c56110d9ff84f78642ae
ANN MARY WILSON <annmarywilson293@gmail.com>
2023-06-03 21:42:16 +0530

    hey

--- backend/model/customer.js
+++ backend/model/customer.js
@@ -1,7 +1,7 @@
 // import mongoose
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://annmarywilson:annmarywilson@cluster0.fwg4655.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://annmarywilson293@gmail.com:annmarywilson03@cluster0.fwg4655.mongodb.net/?retryWrites=true&w=majority')
 
 // to check for error unimportant step
 .then(()=>{
