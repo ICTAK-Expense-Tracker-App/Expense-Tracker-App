9621e9014638398e2395908b09d9e4056aa9c550
ANN MARY WILSON <annmarywilson293@gmail.com>
2023-06-04 22:34:45 +0530

    hry

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,12 +14,12 @@ function App() {
         <Routes>
           <Route path="/Login" element={<Login setLoginUser={setLoginUser} />} />
           <Route path="/SignUp" element={<SignUp />} />
-        {/* <Route path='/' element={<Home/>}/> */}
-          <Route path="/"
+          <Route path='/' element={<Home/>}/>
+          {/* <Route path="/"
             element={
               user && user._id ? <Home setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser} />
-            }
-          /> 
+            }/> */}
+          
         </Routes>
     </div>
   );
