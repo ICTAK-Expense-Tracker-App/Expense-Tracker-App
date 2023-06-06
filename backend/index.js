658d877b0ef646909bed4872a1b8648d6f57a7b4
ANN MARY WILSON <annmarywilson293@gmail.com>
2023-06-06 18:33:06 +0530

    hry

--- backend/index.js
+++ backend/index.js
@@ -1,59 +1,69 @@
 const express = require('express')
-const {model}=require('mongoose')
-const app = new express();
-//const userModel = require('./model/userdb');
-const cors=require('cors')
-
-
-
-app.use(express.urlencoded({extended:true}))
+const cors = require('cors')
+const { collection } = require('./model/user')
+const app = express()
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
-app.post("/Login", (req, res)=> {
-    const { email, password} = req.body
-    User.findOne({ email: email}, (err, user) => {
-        if(user){
-            if(password === user.password ) {
-                res.send({message: "Login Successfull", user: user})
-            } else {
-                res.send({ message: "Password didn't match"})
-            }
-        } else {
-            res.send({message: "User not registered"})
-        }
-    })
+
+
+app.get('/see',async(req,res)=>{
+    var data =  await collection.find()
+    res.json(data)
 })
 
-app.post("/SignUp", (req, res)=> {
-    const { name, place , age ,email , no , password ,reEnterPassword } = req.body
-    User.findOne({email: email}, (err, user) => {
-        if(user){
-            res.send({message: "User already registerd"})
-        } else {
-            const user = new User({
-                name,
-                place,
-                age,
-                email,
-                no,
-                Password,
-                reEnterPassword
-            })
-            user.save(err => {
-                if(err) {
-                    res.send(err)
-                } else {
-                    res.send( { message: "Successfully Registered, Please login now." })
-                }
-            })
+
+app.post("/login",async(req,res)=>{
+    const{email,password}=req.body
+
+    try{
+        const check=await collection.findOne({email:email})
+
+        if(check){
+            res.json("exist")
         }
-    })
-    
+        else{
+            res.json("notexist")
+        }
+
+    }
+    catch(e){
+        res.json("fail")
+    }
+
 })
 
-app.listen(9002,() => {
-    console.log("BE started at port 9002")
+
+
+app.post("/signup",async(req,res)=>{
+    const{email,password}=req.body
+
+    const data={
+        email:email,
+        password:password
+    }
+
+    try{
+        const check=await collection.findOne({email:email})
+
+        if(check){
+            res.json("exist")
+        }
+        else{
+            res.json("notexist")
+            await collection.insertMany([data])
+        }
+
+    }
+    catch(e){
+        res.json("fail")
+    }
+
+})
+
+app.listen(8003,()=>{
+    console.log("port connected")
 })
 
 
--- backend/model/user.js
+++ backend/model/user.js
@@ -1,31 +1,27 @@
-const mongoose = require('mongoose')
-<<<<<<< HEAD:backend/model/user.js
-mongoose.connect('mongodb+srv://annmarywilson293@gmail.com:annmarywilson03@cluster0.fwg4655.mongodb.net/?retryWrites=true&w=majority')
-=======
 
-mongoose.connect('mongodb+srv://annmarywilson:annmarywilson@cluster0.fwg4655.mongodb.net/?retryWrites=true&w=majority')
+const mongoose=require("mongoose")
+mongoose.connect('mongodb+srv://annmarywilson:<password>@cluster0.fwg4655.mongodb.net/Users?retryWrites=true&w=majority')
+    console.log("mongodb connected")
 
-// to check for error unimportant step
->>>>>>> d487f81840bfd7610c8a44c386dd3b36c6b744a2:backend/model/customer.js
-.then(()=>{
+/*.then(()=>{
     console.log("db connected")
 })
-.catch(err=>console.log(err))
-let Schema = mongoose.Schema
-const userSchema = new Schema({
-    name:String,
-    place:String,
-    age:Number,
-    email:String,
-    no:Number,
-    Password:String,
-    reEnterPassword:String
-})
-<<<<<<< HEAD:backend/model/user.js
-const User = new mongoose.model("User", userSchema)
+.catch(()=>{
+    console.log('failed');
+})*/
 
-module.exports = userModel
-=======
-var customerModel = mongoose.model("students",customerSchema)
-module.exports = customerModel;
->>>>>>> d487f81840bfd7610c8a44c386dd3b36c6b744a2:backend/model/customer.js
+
+let newSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+var collection = mongoose.model("collection",newSchema)
+
+module.exports=collection
