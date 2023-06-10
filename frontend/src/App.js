2f1b1a683877a536172b251d1eb710d803fed805
unknown <pearlmj02@gmail.com>
2023-06-10 17:35:25 +0530

    Your commit message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import NavBar from './components/NavBar';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Home from './components/Home';
+import AddNewPage from './components/AddNewPage';
+import ProfilePage from './components/ProfilePage';
 
 function App() {
   const [user, setLoginUser] = useState({});
@@ -16,6 +18,8 @@ function App() {
           <Route path="/Login" element={<Login setLoginUser={setLoginUser} />} />
           <Route path="/SignUp" element={<SignUp />} />
           <Route path='/' element={<Home/>}/>
+          <Route path="/add-new" component={AddNewPage} />
+          <Route path="/profile" component={ProfilePage} />
           {/* <Route path="/"
             element={
               user && user._id ? <Home setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser} />
