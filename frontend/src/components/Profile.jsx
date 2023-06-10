b2a6c6454c17e88d3d4c65b9676573b48000d92c
ANN MARY WILSON <annmarywilson293@gmail.com>
2023-06-10 20:56:31 +0530

    hry

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Profile.css';
- 
-const Profile = () => {
-    return (
-        <div className="profile-container">
-           
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-        </div>
-    );
-}
+const Profile = ({ user }) => {
+  const [profileData, setProfileData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:9002/profile')
+      .then((res) => {
+        setProfileData(res.data.user);
+      })
+      .catch((error) => {
+        console.log('Error occurred while fetching user profile', error);
+      });
+  }, []);
+
+  return (
+    <div className="profile-container">
+      <div className="profile-details">
+        {profileData && (
+          <div>
+            <h2>Profile Details</h2>
+            <p>
+              <strong>Name:</strong> {profileData.name}
+            </p>
+            <p>
+              <strong>Email:</strong> {profileData.email}
+            </p>
+            {/* Add more profile details as needed */}
+          </div>
+        )}
+      </div>
+      <div className="main-content">
+        {/* Add the main content of the profile page */}
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
