457eba2647ef16afa740d57e222990120bc28faf
ANN MARY WILSON <annmarywilson293@gmail.com>
2023-06-15 19:36:31 +0530

    hry

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import SignUp from './components/SignUp';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
 import Profile from './components/Profile';
+import Transactions from './components/Transactions';
 
 function App() {
   const [user, setLoginUser] = useState({});
@@ -26,6 +27,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile userId={user.email} />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/transactions" element={<Transactions />} />
+
       </Routes>
     </div>
   );
