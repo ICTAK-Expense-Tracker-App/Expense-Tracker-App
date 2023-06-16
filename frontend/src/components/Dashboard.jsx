ec29c88994924fe0c5604a019eef422a78a5f913
Ann Mary Wilson <annmarywilson293@gmail.com>
2023-06-16 23:46:32 +0530

    hry

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,28 +1,180 @@
 import React, { useState } from 'react';
 import './Dashboard.css';
-import { Button } from '@mui/material';
-import { Link, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 import Sidebar from './Sidebar';
 import Profile from './Profile';
+import axios from 'axios';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  MenuItem,
+  Select,
+  TextField
+} from '@material-ui/core';
+import './Transactions.css';
 
-const Dashboard = () => {
+const Dashboard = ({userId}) => {
   const [selectedOption, setSelectedOption] = useState('');
 
   const handleOptionSelect = (option) => {
     setSelectedOption(option);
   };
+  const [openDialog, setOpenDialog] = useState(false);
+  const [transactionType, setTransactionType] = useState('');
+  const [transactionAmount, setTransactionAmount] = useState('');
+  const [transactionNote, setTransactionNote] = useState('');
+  const [transactionDate, setTransactionDate] = useState('');
+  const [transactionData, setTransactionData] = useState([]);
+  const [user, setUser] = useState({});
+  const handleOpenDialog = () => {
+    setOpenDialog(true);
+  };
 
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
+ 
+  const handleTransactionTypeChange = (event) => {
+    setTransactionType(event.target.value);
+  };
+
+  const handleTransactionAmountChange = (event) => {
+    setTransactionAmount(event.target.value);
+  };
+
+  const handleTransactionNoteChange = (event) => {
+    setTransactionNote(event.target.value);
+  };
+
+  const handleTransactionDateChange = (event) => {
+    setTransactionDate(event.target.value);
+  };
+ 
+    const fetchTransactions = async () => {
+      try {
+        const response = await axios.get('http://localhost:9002/transactions', {
+          params: { email: userId }, // Replace userEmail with the user's email or userId
+        });
+    
+        if (response.status === 200) {
+          const transactions = response.data;
+          setTransactionData(transactions);
+        } else {
+          console.log('Failed to fetch transactions');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+  
+
+
+const handleAddTransaction = async () => {
+  const newTransaction = {
+    email:userId, // Assuming you have the email stored in the "user.email" variable
+    type: transactionType,
+    amount: transactionAmount,
+    note: transactionNote,
+    date: transactionDate,
+  };
+
+  try {
+    const response = await axios.post('http://localhost:9002/transactions', newTransaction);
+
+    if (response.status === 200) {
+      console.log('Transaction added successfully');
+      const transaction = response.data;
+      setTransactionData([...transactionData, transaction]);
+      alert('Transaction added successfully'); // Show alert
+    } else {
+      console.log('Failed to add transaction');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+
+  handleCloseDialog();
+};
+
+// ...
+
+
+  
   return (
     <div className="dashboard-container">
       <div className='sb'>
       <Sidebar handleOptionSelect={handleOptionSelect} />
       </div>
       <div className="dashboard-content">
-        <Routes>
-          <Route path="/" element={<h2>Welcome to the Dashboard</h2>} />
-          <Route path="/dashboard" element={<h2>Income/Expenses</h2>} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
+          <h1>Income and Expenses</h1>
+          <Button
+            variant="contained"
+            color="primary"
+            className="add-button"
+            onClick={handleOpenDialog}
+          >
+            +
+          </Button>
+
+          <Dialog open={openDialog} onClose={handleCloseDialog}>
+            <DialogTitle>Add New Transaction</DialogTitle>
+            <DialogContent>
+              <Select value={transactionType} onChange={handleTransactionTypeChange}>
+                <MenuItem value="expense">Expense</MenuItem>
+                <MenuItem value="income">Income</MenuItem>
+              </Select>
+              <TextField
+                label="Amount"
+                type="number"
+                value={transactionAmount}
+                onChange={handleTransactionAmountChange}
+              />
+              <TextField
+                label="Note"
+                value={transactionNote}
+                onChange={handleTransactionNoteChange}
+              />
+              <TextField
+                label="Date"
+                type="date"
+                value={transactionDate}
+                onChange={handleTransactionDateChange}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleCloseDialog} color="primary">
+                Cancel
+              </Button>
+              <Button onClick={handleAddTransaction} color="primary">
+                Add
+              </Button>
+            </DialogActions>
+          </Dialog>
+
+          <table>
+            <thead>
+              <tr>
+                <th>Type</th>
+                <th>Amount</th>
+                <th>Note</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {transactionData.map((transaction, index) => (
+                <tr key={index}>
+                  <td>{transaction.type}</td>
+                  <td>{transaction.amount}</td>
+                  <td>{transaction.note}</td>
+                  <td>{transaction.date}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
       </div>
     </div>
   );
